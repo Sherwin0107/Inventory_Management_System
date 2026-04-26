90af3ec3b7f461907dd87336eb2dc9d26b1dc3b1
Sherwin <sherwincari96@gmail.com>
2026-04-26 22:03:25 +0800

    Update database_connection.java

--- inventory_management/database_connection.java
+++ inventory_management/database_connection.java
@@ -1,17 +1,22 @@
 package Inventory_Management;
 
+
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.SQLException;
 
 public class database_connection {
-    private static final String URL = "jdbc:mysql://mysql-ae35f14-project0132.c.aivencloud.com:23579/defaultdb"
-            + "?ssl=true"
-            + "&sslMode=REQUIRED";
-    private static final String USER = "avnadmin";
-    private static final String PASSWORD = "pass here";
-
-    public static Connection getConnection() throws SQLException {
-        return DriverManager.getConnection(URL, USER, PASSWORD);
+    
+    private static final String URL = "jdbc:mysql://localhost:3306/inventory";
+    private static final String USER = "root";
+    private static final String PASSWORD = "pass here"; // your MySQL password here
+    
+    public static Connection getConnection() {
+        try {
+            Connection conn = DriverManager.getConnection(URL, USER, PASSWORD);
+            return conn;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 }
