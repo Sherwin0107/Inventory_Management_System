4a9f370ddbd127978e79a084cff8787c2543bd45
Xedric Andrei <130117297+xedpogi@users.noreply.github.com>
2026-04-26 20:34:20 +0800

    Refactor Main class and implement Table GUI

--- inventory_management/Main.java
+++ inventory_management/Main.java
@@ -1,5 +1,111 @@
-package Inventory_Management;
+package LaCusinaFilipina;
+import java.awt.EventQueue;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import java.awt.Color;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.JScrollPane;
+import javax.swing.JButton;
+public class Table extends JFrame {
+	private static final long serialVersionUID = 1L;
+	private JPanel contentPane;
+	private JTable table;
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Table frame = new Table();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+	/**
+	 * Create the frame.
+	 */
+	public Table() {
+		setBackground(new Color(240, 240, 240));
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setSize(1024, 679);
+		getContentPane().setLayout(null);
+		setVisible(true);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
 
-public class Main {
-oulol
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(0, 0, 1010, 560);
+		contentPane.add(scrollPane);
+
+		//table(as a whole)
+		table = new JTable();
+		table.setBackground(new Color(192, 192, 192));
+		table.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
+		scrollPane.setViewportView(table);
+		table.setModel(new DefaultTableModel(
+			new Object[][] {
+				{null, null, null, null, null, null, null, null, null},
+				{null, null, null, null, null, null, null, null, null},
+				{null, null, null, null, null, null, null, null, null},
+				{null, null, null, null, null, null, null, null, null},
+				{null, null, null, null, null, null, null, null, null},
+				{null, null, null, null, null, null, null, null, null},
+				{null, null, null, null, null, null, null, null, null},
+				{null, null, null, null, null, null, null, null, null},
+				{null, null, null, null, null, null, null, null, null},
+				{null, null, null, null, null, null, null, null, null},
+				{null, null, null, null, null, null, null, null, null},
+				{null, null, null, null, null, null, null, null, null},
+				{null, null, null, null, null, null, null, null, null},
+				{null, null, null, null, null, null, null, null, null},
+				{null, null, null, null, null, null, null, null, null},
+				{null, null, null, null, null, null, null, null, null},
+				{null, null, null, null, null, null, null, null, null},
+				{null, null, null, null, null, null, null, null, null},
+				{null, null, null, null, null, null, null, null, null},
+				{null, null, null, null, null, null, null, null, null},
+				{null, null, null, null, null, null, null, null, null},
+				{null, null, null, null, null, null, null, null, null},
+			},
+			new String[] {
+				"Category", "Ingredient", "<html>Current<br>Quantity</html>", "<html>Daily<br>Need (pcs)</html>", "<html>Weekly<br>Need (pcs)</html>", "<html>Monthly<br>Need (pcs)</html>", "<html>Yearly<br>Need (pcs)</html>", "<html>Days<br>Remaining</html>", "Status"
+			}
+		) {
+			Class[] columnTypes = new Class[] {
+				String.class, String.class, Integer.class, Integer.class, Integer.class, Integer.class, Integer.class, Integer.class, Object.class
+			};
+			public Class getColumnClass(int columnIndex) {
+				return columnTypes[columnIndex];
+			}
+		});
+		table.getColumnModel().getColumn(0).setPreferredWidth(130);
+		table.getColumnModel().getColumn(1).setPreferredWidth(150);
+		table.getColumnModel().getColumn(2).setPreferredWidth(100);
+		table.getColumnModel().getColumn(3).setPreferredWidth(90);
+		table.getColumnModel().getColumn(4).setPreferredWidth(100);
+		table.getColumnModel().getColumn(5).setPreferredWidth(100);
+		table.getColumnModel().getColumn(6).setPreferredWidth(90);
+		table.getColumnModel().getColumn(7).setPreferredWidth(130);
+		table.getColumnModel().getColumn(8).setPreferredWidth(90);
+		table.getTableHeader().setPreferredSize(
+		    new java.awt.Dimension(0, 60)
+		);
+		table.setRowHeight(25);
+		
+		JButton btnNewButton = new JButton("Add");
+		btnNewButton.setBounds(755, 570, 84, 20);
+		contentPane.add(btnNewButton);
+		
+		JButton btnNewButton_1 = new JButton("Remove\t\r\n");
+		btnNewButton_1.setBounds(849, 570, 84, 20);
+		contentPane.add(btnNewButton_1);
+	}
 }
